d82b1ce973e64b48154fc8a1d5a1c041d17a4937
Moji-Dardar <71829164+Moji-Dardar@users.noreply.github.com>
2021-03-08 06:46:05 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(2020, 3, 8, 15).getHours();
+
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+      {" "}
+      {
+        // isLoggedIn ? <h1>Hello</h1> : <Login />
+        currentTime > 12 && <h1>Why are you still working?</h1>
+      }
+    </div>
+  );
+}
+
+export default App;
